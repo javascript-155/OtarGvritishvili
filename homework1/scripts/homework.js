72145cf56ccf84cb02c2ccb8a7e7dcf85f3c0032
OtarGvritishvili <otargvritishvili@gmail.com>
2023-11-02 13:21:28 +0400

    Homework1
    Adding my homework 1.

---
+++ homework1/scripts/homework.js
@@ -0,0 +1,6 @@
+let firstName = prompt("What's your name?");
+let birth = prompt("Year of your birth?");
+let age = 2023 - birth;
+let place = prompt("Where do you live?");
+
+alert("Welcome " + firstName + " I am happy because you are " + age + " years old and you live in " + place);
