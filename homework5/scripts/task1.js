a5686fe0c2bfaca9865d2d69cdf7139c9a12e4ab
OtarGvritishvili <otargvritishvili@gmail.com>
2023-11-15 11:35:45 +0400

    homework5
    Updating homework 5.

--- homework5/scripts/task1.js
+++ homework5/scripts/task1.js
@@ -2,51 +2,27 @@
 შექმენით ფუნქცია, რომელიც იპოვის ყველზე გრძელ სიტყვას მომხმარებლის მიერ შეყვანილ სტრიქონში. (გამოიყენეთ prompt()-მონაცემების შესაყვანად.)
 */
 
-let string = "i love gam";
+let string = prompt("Please enter some words and I will return to you which of them is the longest");
 let stringToArray = string.split(" ");
-let main = [];
-let mainMain= [];
+let main = [" "];
+let equalMain = [];
 
 
-console.log(stringToArray);
+let finder = stringToArray.forEach(function (index) {
 
-let finder = stringToArray.filter(function (index) {
-    // console.log(index.length);
-    if (index.length > index.length) {
-        main.unshift(index);
-    }
+    if (index.length > main[0].length) {
+        main.push(index);
+        main.shift();
+    } else if (index.length = main[0].length){
+        equalMain.push(index);
+    };
+
 });
-console.log(main);
-// console.log(recycle);
 
 
-
-
-// else if (index.length === 0) {
-//     return alert("please enter some words!");
-// } else if (index.length > main[0].length && index.length === index.length) {
-//     main.unshift(index);
-//     main.pop();
-// } else if (index.length === main[0].length) {
-//     main.unshift(index);
-// } 
-
-
-
-
-
-
-
-// function finder () {
-//     count = 0;
-//     while (count < stringToArray.length) {
-//         let eachArrayes = stringToArray[count];
-//         let eachArrayLengthes = eachArrayes.length;
-//         console.log(eachArrayLengthes);
-
-//         count = count + 1;
-//     }
-// }
-//     finder();
-
-
+let finder2 = equalMain.forEach(function (index) {
+    if (index.length === main[0].length) {
+        main.push(index);
+    }
+})
+alert("Longest word(s): " + main.join(' , '));
