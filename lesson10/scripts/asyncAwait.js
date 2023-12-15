f6c3500a47c1dd309f7ece39077fc2ef4ed41d26
OtarGvritishvili <otargvritishvili@gmail.com>
2023-12-15 12:22:11 +0400

    lesson10
    Update.

--- lesson10/scripts/asyncAwait.js
+++ lesson10/scripts/asyncAwait.js
@@ -4,27 +4,28 @@ async function engHolidays() {
     let response = await fetch('https://www.gov.uk/bank-holidays.json');
     let data = await response.json();
 
+
     let holidays = data['england-and-wales'].events;
     let holidaysTable = document.getElementById('holidaysTable');
+
 
     holidays.forEach(holiday => {
         let newTr = document.createElement('tr');
         let newTd = document.createElement('td');
         newTd.innerHTML = holiday.title;
-        newTd.appendChild(newTd);
+        newTr.appendChild(newTd);
 
-        let newTd2 = document.getElementById('td');
+        let newTd2 = document.createElement('td');
         newTd2.innerHTML = holiday.notes;
         newTr.appendChild(newTd2);
         holidaysTable.appendChild(newTr);
 
-        let newTd3 = document.getElementById('td');
-        newTd2.innerHTML = holiday.notes;
+        let newTd3 = document.createElement('td');
+        newTd3.innerHTML = holiday.date;
         newTr.appendChild(newTd3);
         holidaysTable.appendChild(newTr);
-     })
 
-    console.log(holidays);
+    });
+    console.log(holidays)
     return holidays;
-
 }
